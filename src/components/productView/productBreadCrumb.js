f9fc710bd2b21ea55c8d3db9193e3d1c3c5b748e
iamNarcisse <mechikaegon@gmail.com>
2019-01-15 15:01:03 +0100

    Adjusted breadcrumb

--- src/components/productView/productBreadCrumb.js
+++ src/components/productView/productBreadCrumb.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class ProductBreadCrumb extends Component {
 
     render() {
         return (
             <div id="breadcrumb" className="section">
                 <div className="container">
-                <div className="row">
-					<div className="col-md-12">
-						<ul className="breadcrumb-tree">
-							<li><a href="#home">Home</a></li>
-                <li><a href="#Categories">All Categories</a></li>
-                <li><a href="#Accessories">Accessories</a></li>
-                <li><a href="#headphones">Food</a></li>
-                <li className="active"></li>
-						</ul >
-					</div >
-				</div >
-            
-			</div >
-		</div >
-		
+                    <div className="row">
+                        <div className="col-md-12">
+                            <ul className="breadcrumb-tree">
+                                <li><Link to="/">Home</Link></li>
+                                {/*<li><a href="#Categories">All Categories</a></li>*/}
+                                { /*<li><a href="#Accessories">Accessories</a></li>*/}
+                                { /*<li><a href="#headphones">Food</a></li>*/}
+                                <li className="active"></li>
+                            </ul >
+                        </div >
+                    </div >
+
+                </div >
+            </div >
+
         )
     }
 }
