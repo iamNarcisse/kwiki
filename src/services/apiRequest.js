4d8ff965da361074dfa387f5873f73c2ad0478aa
iamNarcisse <mechikaegon@gmail.com>
2019-01-15 01:49:33 +0100

    set back api url to production

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -24,8 +24,8 @@ const adminConfig = {
 
 //Naricsse
 //Don't forget to comment http://localhost:3000 during production
-//const apiUrl = 'https://arcane-shelf-55983.herokuapp.com';
-const apiUrl = 'http://localhost:3000';
+const apiUrl = 'https://arcane-shelf-55983.herokuapp.com';
+//const apiUrl = 'http://localhost:3000';
 const productApiUrl = `${apiUrl}/products`;
 const authApiUrl = `${apiUrl}/auth`;
 const userApiUrl = `${apiUrl}/users`;
